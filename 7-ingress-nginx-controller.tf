a0326a2ba79bf4e4f33c685f2aa9a91037b91ce6
Shuhai Liao <shuai190060roy@gmail.com>
2023-05-22 15:51:22 +0200

    install nginx ingress controller with helm

---
+++ 7-ingress-nginx-controller.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "nginx_ingress_controller" {
+  name = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  version = "4.4.2"
+  values = [ "${file("ingress-nginx-values.yaml")}" ]
+
+  namespace = "ingress"
+  create_namespace = true
+
+}
