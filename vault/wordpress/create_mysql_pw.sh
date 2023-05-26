1cd59c2220abf0f733504900c10e8668719747cc
Shuhai Liao <shuai190060roy@gmail.com>
2023-05-26 15:01:29 +0200

    create secret and delete helper busybox

--- vault/wordpress/create_mysql_pw.sh
+++ vault/wordpress/create_mysql_pw.sh
@@ -9,5 +9,8 @@ VALUE=$(echo "$SECRET_FILE" | jq -r '.password')
 kubectl create secret generic wordpress-secret --namespace=app --from-literal=mysql-password=$VALUE
 
 # create file for the secret manifest
-touch ./vault/test-1/wordpress-secret.yaml
-kubectl get secret/wordpress-secret -n app -o yaml > ./vault/test-1/wordpress-secret.yaml
+touch ./vault/wordpress/wordpress-secret.yaml
+kubectl get secret/wordpress-secret -n app -o yaml > ./vault/wordpress/wordpress-secret.yaml
+
+# delete the busybox helper
+kubectl delete  -n app -f vault/wordpress/1-busybox.yaml
