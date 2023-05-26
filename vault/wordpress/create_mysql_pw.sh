35d29d671234d82d8b7e344ef336c416472e8c0b
Shuhai Liao <shuai190060roy@gmail.com>
2023-05-26 14:02:16 +0200

    secret ingestion for wordpress

---
+++ vault/wordpress/create_mysql_pw.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# this is script to call the secret injected in the busybox, and create secret object out of it for wordpress
+
+echo "create secret object for wordpress"
+POD_NAME=$(kubectl -n app get pods -l app=busybox-mysql-secret -o jsonpath='{.items[0].metadata.name}')
+SECRET_FILE=$(kubectl -n app exec "$POD_NAME" -- sh -c 'cat /vault/secrets/mysql')
+VALUE=$(echo "$SECRET_FILE" | jq -r '.password')
+kubectl create secret generic wordpress-secret --namespace=app --from-literal=mysql-password=$VALUE
+
+# create file for the secret manifest
+touch ./vault/test-1/wordpress-secret.yaml
+kubectl get secret/wordpress-secret -n app -o yaml > ./vault/test-1/wordpress-secret.yaml
