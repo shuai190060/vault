569d2cf482890f01a91aa27db27b1f9a17e97f1c
Shuhai Liao <shuai190060roy@gmail.com>
2023-06-02 10:59:17 +0200

    comment out for optional deploy

--- 11-vault_helm.tf
+++ 11-vault_helm.tf
@@ -1,22 +1,22 @@
-resource "helm_release" "vault" {
-  name       = "vault"
-  namespace  = "vault"
-  repository = "hashicorp"
-  chart      = "vault"
-  version    = "0.24.1"
-  set {
-    name  = "server.ha.enabled"
-    value = "true"
-  }
-  set {
-    name  = "server.ha.raft.enabled"
-    value = "true"
-  }
+# resource "helm_release" "vault" {
+#   name       = "vault"
+#   namespace  = "vault"
+#   repository = "hashicorp"
+#   chart      = "vault"
+#   version    = "0.24.1"
 #   set {
-#     name  = "csi.enabled"
+#     name  = "server.ha.enabled"
 #     value = "true"
 #   }
-  create_namespace = true
+#   set {
+#     name  = "server.ha.raft.enabled"
+#     value = "true"
+#   }
+# #   set {
+# #     name  = "csi.enabled"
+# #     value = "true"
+# #   }
+#   create_namespace = true
 
-}
+# }
 
--- 12-sealed-secrets.tf
+++ 12-sealed-secrets.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "sealed-secrets" {
-  name       = "sealed-secrets"
-  namespace  = "kube-system"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "sealed-secrets"
-  version    = "1.4.2"
+# resource "helm_release" "sealed-secrets" {
+#   name       = "sealed-secrets"
+#   namespace  = "kube-system"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "sealed-secrets"
+#   version    = "1.4.2"
 
 
-}
+# }
