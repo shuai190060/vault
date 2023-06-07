f3c5c0f32dbef07070d84a0f54b1cb576bd19fc6
Shuhai Liao <shuai190060roy@gmail.com>
2023-06-07 09:52:09 +0200

    prometheus

--- 8-prometheus.tf
+++ 8-prometheus.tf
@@ -11,16 +11,16 @@ resource "helm_release" "prometheus-stack" {
 }
 
 
-resource "helm_release" "cadvisor" {
-  name      = "cadvisor"
-  repository = "https://ckotzbauer.github.io/helm-charts"
-  chart     = "cadvisor"
-  version   = "2.2.4"
-  namespace = "monitoring"
+# resource "helm_release" "cadvisor" {
+#   name      = "cadvisor"
+#   repository = "https://ckotzbauer.github.io/helm-charts"
+#   chart     = "cadvisor"
+#   version   = "2.2.4"
+#   namespace = "monitoring"
 
-  values = <<EOF
-  serviceMonitor:
-    additionalLabels: 
-      prometheus: watched
-  EOF
-}
+#   values = <<EOF
+#   serviceMonitor:
+#     additionalLabels: 
+#       prometheus: watched
+#   EOF
+# }
